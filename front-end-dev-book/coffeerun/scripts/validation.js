a416bcc3f9a87f71af60f01e9800b39abfbd5448
Sami Halwani <4sakenmage1@csu.fullerton.edu>
2019-10-07 23:54:14 -0700

    Chapter12 and 13 completed

---
+++ front-end-dev-book/coffeerun/scripts/validation.js
@@ -0,0 +1,20 @@
+(function (window) {
+  "use strict";
+  var App = window.App || {};
+  var Validation = {
+  };
+  App.Validation = Validation;
+  window.App = App;
+})(window);
+
+(function () {
+  "use strict";
+  var App = window.App || {};
+  var Validation = {
+    isCompanyEmail: function (email) {
+      return /.+@bignerdranch\.com$/.test(email);
+    }
+  };
+  App.Validation = Validation;
+  window.App = App;
+})(window);
